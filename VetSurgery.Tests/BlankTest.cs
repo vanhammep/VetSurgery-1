e42ba5e9e58e7b2b83188fb5ac3adef25b3d68d9
Gary Moore <garyprm@hotmail.com>
2017-02-12 22:34:14 +0000

    Added domain project and blank test

---
+++ VetSurgery.Tests/BlankTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetSurgery.Tests
+{
+    [TestFixture]
+    public class BlankTest
+    {
+        [Test]
+        public void RunTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
